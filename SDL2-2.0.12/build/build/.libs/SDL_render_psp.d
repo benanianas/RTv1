157f8bef2b428c83d7d7aa4bf2b4253400283b1f
anas <anas@anas.co>
2020-12-17 19:16:03 +0100

    makefile edited

--- SDL2-2.0.12/build/build/.libs/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/src/render/psp/SDL_render_psp.c \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/src/render/psp/../../SDL_internal.h \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/src/render/psp/../../dynapi/SDL_dynapi.h \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/include/SDL_platform.h \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/include/begin_code.h \
-  /Users/abenani/Desktop/RTv1/SDL2-2.0.12/include/close_code.h
