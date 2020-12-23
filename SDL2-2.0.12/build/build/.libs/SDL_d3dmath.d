439181e4c79220761c7ae532acde6992faafc3c9
benanianas <benanianas21@gmail.com>
2020-12-23 18:27:17 +0100

    editing main

--- SDL2-2.0.12/build/build/.libs/SDL_d3dmath.d
+++
@@ -1,9 +0,0 @@
-build/SDL_d3dmath.lo: \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/SDL_d3dmath.c \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/../SDL_internal.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/../dynapi/SDL_dynapi.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/SDL_platform.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/begin_code.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/close_code.h
--- SDL2-2.0.12/build/build/SDL_render_d3d11.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_d3d11.lo: \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/direct3d11/SDL_render_d3d11.c \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/direct3d11/../../SDL_internal.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/direct3d11/../../dynapi/SDL_dynapi.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/src/render/direct3d11/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/SDL_platform.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/begin_code.h \
- /home/anas/Desktop/RTv1/SDL2-2.0.12/include/close_code.h
