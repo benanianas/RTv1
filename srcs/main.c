bc4f197fcd2dd597469f670d3e81646d6a45af9a
benanianas <benanianas21@gmail.com>
2021-01-15 13:19:01 +0100

    sdl functions

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abenani <abenani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abenani <abenani@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 11:37:19 by abenani           #+#    #+#             */
-/*   Updated: 2021/01/13 19:00:39 by abenani          ###   ########.fr       */
+/*   Updated: 2021/01/15 13:18:22 by abenani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,19 @@ t_obj parser(int ac, char **av)
     return (object);
 }
 
+int    startSdl(t_sdl *sdl)
+{
+    if(!SDL_Init(SDL_INIT_VIDEO))
+        if(sdl->win = SDL_CreateWindow("RTv1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W_WIDTH, W_HEIGHT, SDL_WINDOW_SHOWN))
+            if(sdl->rend = SDL_CreateRenderer(sdl->win, -1, SDL_RENDERER_ACCELERATED))
+                return 1;
+            else
+                SDL_DestroyWindow(sdl->win);
+        else
+           SDL_Quit();
+    return 0;
+}
+
 int exitSdl(t_sdl sdl)
 {
     SDL_Event e;
@@ -61,6 +74,24 @@ int exitSdl(t_sdl sdl)
     return 0;
 }
 
+void draw(SDL_Renderer    *rend)
+{
+    // SDL_SetRenderDrawBlendMode(rend, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(rend, 0, 0, 255, 50);
+    int i = 0;
+    while (i < W_HEIGHT)
+    {
+        int j = 0;
+        while(j < W_WIDTH)
+        {
+            SDL_RenderDrawPoint(rend, j, i);
+            j++;
+        }
+        i++;
+    }
+    SDL_RenderPresent(rend);
+}
+
 int main(int ac, char **av)
 {
     // t_obj object;
@@ -71,14 +102,18 @@ int main(int ac, char **av)
 
     t_sdl sdl;
     
-    SDL_Init(SDL_INIT_VIDEO);
-    sdl.win = SDL_CreateWindow("RTv1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W_WIDTH, W_HEIGHT, SDL_WINDOW_OPENGL);
-    sdl.rend = SDL_CreateRenderer(sdl.win, -1, SDL_RENDERER_ACCELERATED  | SDL_RENDERER_PRESENTVSYNC);
+    if(!startSdl(&sdl))
+    {
+        ft_putendl(SDL_GetError());
+        exit (0);
+    }
+    //here the rendering function (returns a table of colors)
 
-     SDL_SetRenderDrawColor(sdl.rend, 0, 255, 0, 255);
-     SDL_RenderDrawPoint(sdl.rend, 0, 0);
-    SDL_RenderPresent(sdl.rend);
+    // pass the tabele to draw
+
     
+    draw(sdl.rend);
+
     while(1)
         if(exitSdl(sdl))
             break;
