116dda2bd623d909ab0ad547d64ad89a8adac748
benanianas <benanianas21@gmail.com>
2021-01-29 13:45:47 +0100

    colors

--- SDL2-2.0.12/build/build/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/src/render/psp/SDL_render_psp.c \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/src/render/psp/../../SDL_internal.h \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/src/render/psp/../../dynapi/SDL_dynapi.h \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/include/SDL_platform.h \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/include/begin_code.h \
- /home/anas/Desktop/lastRTv1/SDL2-2.0.12/include/close_code.h
